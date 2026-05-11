3f5cea9e523c45271a51ffeb6a509e23ec417353
Javi <javieradelcarmen.ojeda@alumnos.ulagos.cl>
2026-05-11 12:01:06 -0400

    Subiendo archivo de variables de python

---
+++ 01-variables.py
@@ -0,0 +1,29 @@
+nombre = 'Javiera' 
+apellido = 'O'
+edad = 0
+# comentario
+"""Comentario 
+multilinea"""
+
+#formas de imprimir texto
+# Forma 1: separando variables y texto por comas
+print("Mi nombre es", nombre, apellido, "y tengo", edad, "años")
+
+# Forma 2: f-strings
+print(f"mi nombre es {nombre} y mi apellido es {apellido} y tengo {edad} años")
+
+# Forma 3: concatenación
+print("mi nombre es " + nombre + " y mi apellido es " + apellido + " y tengo " + str(edad) + " años ")
+
+# La función str transforma el valor a Cadena de texto
+
+""" llaves = {}
+Parentesis = ()
+Corchetes  = []"""
+
+# Utilizar metodo o función Input 
+
+carrera = input ("Que carrera estudias?")
+print(f"yo estudio la carrera de:  {carrera}")
+
+
