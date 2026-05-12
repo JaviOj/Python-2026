2ea87e574b3132d36ed7137f28f3cb4bc72c3b72
Javi <javieradelcarmen.ojeda@alumnos.ulagos.cl>
2026-05-12 17:01:04 -0400

    Subiendo archivo de listas

---
+++ 03-listas.py
@@ -0,0 +1,13 @@
+print("-----LISTAS-----")
+
+# Primera forma de declarar listas
+lista1 = ["a", 6, True]
+# Segunda forma de declarar listas
+n = list([1,2,3,4,5,6])
+
+print(type(n))
+print(type(lista1))
+
+# Metodo listas
+# 01-Count() - contar la cantidad de concurrencias de un elemento
+print(lista1.count(6))
