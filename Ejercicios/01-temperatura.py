0bb9d7612539346a2b269c25948bc16edff51ed9
Javi <javieradelcarmen.ojeda@alumnos.ulagos.cl>
2026-05-26 16:01:39 -0400

    mejorando ortografia

---
+++ Ejercicios/01-temperatura.py
@@ -0,0 +1,7 @@
+temperaturas = [12.5,14.2,11.8]
+promedio = ((temperaturas[0] + (temperaturas[1]) + (temperaturas[2]))/3) 
+print(f"El promedio de las 3 temperaturas es de {promedio:.2f}°")
+temperaturas.sort()
+
+diferencia = (temperaturas[2])-(temperaturas[0])
+print(f"La diferencia es de {diferencia:.2f}°")
