e735ab8608c59dd088861c1f4734446d5c35a9b6
Javi <javieradelcarmen.ojeda@alumnos.ulagos.cl>
2026-05-28 16:04:43 -0400

    Agregando funcion LEN

--- Ejercicios/01-temperatura.py
+++ Ejercicios/01-temperatura.py
@@ -1,5 +1,5 @@
 temperaturas = [12.5,14.2,11.8]
-promedio = sum(temperaturas)/3
+promedio = (sum(temperaturas))/len(temperaturas)
 print(f"El promedio de las 3 temperaturas es de {promedio:.2f}°")
 diferencia = (temperaturas[1])-(temperaturas[2]) #se puede usar max()min() 
 print(f"La diferencia es de {diferencia:.2f}°")
