5457a58d5e63592728f6b15a30f850e455b197e1
Javi <javieradelcarmen.ojeda@alumnos.ulagos.cl>
2026-06-02 14:40:26 -0400

    Ortografia

--- Ejercicios/01-temperatura.py
+++ Ejercicios/01-temperatura.py
@@ -3,4 +3,3 @@ promedio = (sum(temperaturas))/len(temperaturas)
 print(f"El promedio de las 3 temperaturas es de {promedio:.2f}°")
 diferencia = (temperaturas[1])-(temperaturas[2]) #se puede usar max()min() 
 print(f"La diferencia es de {diferencia:.2f}°")
-
