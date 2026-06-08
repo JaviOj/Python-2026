b94f1e8dc0c2d9d648e8845eaec199c2b10a5678
Javi <javieradelcarmen.ojeda@alumnos.ulagos.cl>
2026-06-08 13:00:11 -0400

    Diccionarios

---
+++ 06-diccionario.py
@@ -0,0 +1,12 @@
+print(f"-----------------SETS-----------------")
+
+#DICCIONARIOS
+paciente = {
+    "Nombre" : "Lorena",
+    "Edad" : 29,
+    "Ciudad" : "Ancud",
+    "fechas atencion" : [5,8,12]
+} #Es recomendable usar los diccionarios con espaciado para no confundirlos con sets
+
+print(type(paciente))
+print(f"===FICHA PACIENTE=====\n{paciente}")
