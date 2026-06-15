680bd81118225432d3eeb3f26e29ac6d20dadbde
Javi <javieradelcarmen.ojeda@alumnos.ulagos.cl>
2026-06-15 12:42:07 -0400

    Agregando operadores aritmeticos

---
+++ 07-operadores.py
@@ -0,0 +1,20 @@
+# Operadores en python
+a = 10
+b = 5
+c = 4
+d = 10
+print("===OPERADORES ARITMETICOS===")
+print(f"La suma entre la variable a y b es: {a+b}")
+print(f"La resta entre la variable a y b es: {a-b}")
+print(f"La multiplicacion entre la variable a y b es: {a*b}")
+print(f"La división entre la variable a y b es: {a/b}")
+print(f"El módulo entre la variable a y b es: {a%b}")
+print(f"El coeficiente entre la variable b y c es: {b//c}")
+print(f"El resultado entre la variable b elevado a c es: {b ** c}")
+
+# Se puede hacer esta operacion?
+print("Hola" * ((int(10*2)/5)),"\n")
+print("\n===OPERADORES DE COMPARACIÓN===") # Booleano
+print(a == b)
+
+
