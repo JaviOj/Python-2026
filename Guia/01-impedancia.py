6f3dff91c3fcc8543133dbae6a8e6acfe65a1b76
Javi <javieradelcarmen.ojeda@alumnos.ulagos.cl>
2026-06-20 18:56:44 -0400

    eliminado

--- Guia/01-impedancia.py
+++
@@ -1,8 +0,0 @@
-impedancia1 = 50 + 30j
-impedancia2 = 40 - 10j
-sumaimpedancias = impedancia1 + impedancia2
-print(sumaimpedancias)
-impedanciareal = int(sumaimpedancias.real)
-print(impedanciareal)
-impedanciaimag = int(sumaimpedancias.imag)
-print(impedanciaimag)
