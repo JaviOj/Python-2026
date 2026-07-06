49c63be279257a749cad643527c0c86ce9b539b1
Javi <javieradelcarmen.ojeda@alumnos.ulagos.cl>
2026-07-06 11:05:14 -0400

    Subiendo doc. funciones

---
+++ 12-funciones.py
@@ -0,0 +1,14 @@
+
+# Ejemplo de función simple
+def multiplicacion(x):# parametro puede tomar cualquier valor
+    return x * 10 
+
+y = multiplicacion(20) # Utilizandolo en variable
+print(f"El resultado de la función es {y}")
+
+# Segundo ejemplo
+def saludo(nombre):
+    print(f"Hola mi nombre es {nombre}")
+
+saludo("Tomas")
+
